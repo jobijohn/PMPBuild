74a38288bab545005bd7f9ac598998083cffa496
jobijohn <jobikjohn@gmail.com>
2016-10-22 18:15:46 +0530

    issue create

--- jiraIssue.js
+++ jiraIssue.js
@@ -2,11 +2,23 @@
  * Created by jobi on 10/8/16.
  */
 
-var async =  require('async'),
+var common =  require('./common'),
+    async =  require('async'),
     fs = require('fs'),
     child_process = require('child_process');
 function renderForm(req, res) {
-    res.render('jiraIssue');
+    var indexData = {};
+    var issueJsonFile = 'jiraissues.json';
+    common.readJsonFile(issueJsonFile, function (err, issueData) {
+    common.getAllUniqueDistricts(issueData, function (err, districts) {
+        indexData.districts = districts;
+        common.getAllUniqueTaluks(issueData, function (err, taluks) {
+            indexData.taluks = taluks;
+            res.render('jiraIssue', {indexData:indexData});
+        });
+    });
+    });
+
 }
 
 function randomFileName() {
@@ -18,10 +30,14 @@ function randomFileName() {
 
 function createJiraIssue(req, res) {
     var issueType = req.param('ap-issuetype');
+    var summary = req.param('ap-summary');
+    var name = req.param('ap-name');
     var district = req.param('ap-district');
     var taluk = req.param('ap-taluk');
+    var seedName = req.param('ap-seed-name');
+    var seedQty = req.param('ap-seed-qty');
     var acres = req.param('ap-acres');
-    console.log('console ', issueType, district, taluk, acres);
+    //console.log('console ', issueType, district, taluk, acres);
     var randomFile  = randomFileName();
     randomFile = randomFile + '.json';
     async.waterfall([
@@ -34,7 +50,22 @@ function createJiraIssue(req, res) {
         },
         function (randomFile, callback) {
             var obj = require("./"+ randomFile.toString());
-            obj.fields.summary = "Check Insert";
+            obj.fields.issuetype = {};
+            obj.fields.issuetype.id = parseInt(issueType);
+            obj.fields.summary = summary;
+
+            obj.fields.customfield_10400 = {};
+            obj.fields.customfield_10400.value = district;
+
+            obj.fields.customfield_10401 = {};
+            obj.fields.customfield_10401.value = taluk;
+
+            obj.fields.customfield_10404 = {};
+            obj.fields.customfield_10404.value = seedName;
+
+            obj.fields.customfield_10405 = parseInt(seedQty);
+            obj.fields.customfield_10403 = parseInt(acres);
+
             fs.writeFile('./' + randomFile, JSON.stringify(obj), function (err) {
                 callback(null, randomFile);
             });
@@ -47,17 +78,26 @@ function createJiraIssue(req, res) {
                 return result;
             }
 
-            var cmd = "curl -u jobikjohn:jkjjkj -X POST --data @" + randomFile + "-H 'Content-Type: application/json' https://swarmact.atlassian.net/rest/api/2/issue/";
+            var cmd = "curl -u jobikjohn:jkjjkj -X POST --data @" + randomFile.toString() + " -H 'Content-Type: application/json' https://swarmact.atlassian.net/rest/api/2/issue/";
             var result = runCmd(cmd);
+
+            console.log('randomFile:' + randomFile);
+            console.log('result:' + result);
             callback(null, randomFile);
         },
         function (randomFile, callback) {
-            // fs.unlink(randomFile, function (err) {
+             fs.unlink(randomFile, function (err) {
                 callback(null, randomFile);
-            // });
+             });
         }
         ],function (err, result) {
-            console.log('File Removed -', result);
+            //console.log('File Removed -', result);
+            if(err){
+                res.send({success:false});
+            } else {
+                res.send({success:true});
+            }
+
     });
 }
 
